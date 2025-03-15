fdcf99b60d20bd1e9ef9465bc96f00e5a0733629
jhonangelfontaines104 <jhonangelfontaines104@gmail.com>
2025-03-15 06:21:04 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  double a = 2.5;
+  double b = 3.7;
+  double c = 1.9;
+  double d = 4.2;
+
+  std::cout << "a + b = " << a + b << std::endl;
+  std::cout << "c - d = " << c - d << std::endl;
+  std::cout << "a * b = " << a * b << std::endl;
+  std::cout << "c / d = " << c / d << std::endl;
+  std::cout << "sqrt(d) = " << sqrt(d) << std::endl;
+}
