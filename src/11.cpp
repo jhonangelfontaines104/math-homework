a273ac5fa7d4d648bdc198c03a5a9a425175d408
jhonangelfontaines104 <jhonangelfontaines104@gmail.com>
2025-03-17 23:25:17 +0000

    Add 11.cpp

---
+++ src/11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int a = 5;
+    int b = 2;
+    int c = a + b;
+
+    std::cout << "The sum of " << a << " and " << b << " is " << c << std::endl;
+
+    return 0;
+}
