d54ce9bb2e77759d7726a81b168b90c198cf718b
jhonangelfontaines104 <jhonangelfontaines104@gmail.com>
2025-03-18 15:24:02 +0000

    Add 12.cpp

---
+++ src/12.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+    std::cout << "Their sum is: " << num1 + num2 << std::endl;
+    return 0;
+}
