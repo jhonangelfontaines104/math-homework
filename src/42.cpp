948475d0d5166fb6f45bdefadbe4e049318fe25f
jhonangelfontaines104 <jhonangelfontaines104@gmail.com>
2025-05-19 11:14:13 +0000

    Add 42.cpp

---
+++ src/42.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    float result = a / b;
+    cout << "The result is: " << result << endl;
+    return 0;
+}
